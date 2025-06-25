75cd14f87e877654bcddd4d5707783d55471cea2
hectorhm98 <55853437+hectorhm98@users.noreply.github.com>
2025-06-25 16:59:33 +0200

    Initial Commit

---
+++ RandomUserChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RandomUserChallenge
+//
+//  Created by Hector Hernandez Montilla on 25/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RandomUserChallenge/RandomUserChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserChallengeApp.swift
+//  RandomUserChallenge
+//
+//  Created by Hector Hernandez Montilla on 25/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
